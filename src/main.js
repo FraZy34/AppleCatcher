d7950ab2b9be41be279b55b35589d9fb1ec23030
Mathéo Seurat <matheo.seurat@etudiant.univ-reims.fr>
2025-10-15 18:09:04 +0200

    Ajout de mon jeu AppleCatcher

---
+++ src/main.js
@@ -0,0 +1,122 @@
+import './style.css'
+import Phaser from 'phaser'
+
+const sizes = { width: 500, height: 500 }
+const speedDown = 500;
+
+const gameCanvas = document.querySelector("#gameCanvas");
+const gameStartDiv = document.querySelector("#gameStartDiv");
+const gameStartBtn = document.querySelector("#gameStartBtn");
+const gameEndDiv = document.querySelector("#gameEndDiv");
+const gameWinLoseSpan = document.querySelector("#gameWinLoseSpan");
+const gameEndScoreSpan = document.querySelector("#gameEndScoreSpan");
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("scene-game");
+    this.player;
+    this.cursor;
+    this.playerSpeed = speedDown + 50;
+    this.target;
+    this.points = 0;
+    this.textScore;
+    this.textTime;
+    this.timedEvent;
+    this.remainingTime;
+  }
+
+  preload() {
+    this.load.image("bg", "assets/bg.png");
+    this.load.image("basket", "assets/basket.png");
+    this.load.image("apple", "assets/apple.png");
+  }
+
+  create() {
+
+    this.add.image(0, 0, "bg").setOrigin(0, 0);
+
+    this.player = this.physics.add.image(0, sizes.height - 100, "basket").setOrigin(0, 0);
+    this.player.setImmovable(true);
+    this.player.body.allowGravity = false;
+    this.player.setCollideWorldBounds(true);
+    this.player.setSize(this.player.width - this.player.width / 4, this.player.height / 6)
+      .setOffset(this.player.width / 10, this.player.height - this.player.height / 5);
+
+    this.target = this.physics.add.image(0, 0, "apple").setOrigin(0, 0);
+    this.target.setMaxVelocity(0, speedDown);
+
+    this.physics.add.overlap(this.target, this.player, this.targetHit, null, this);
+
+    this.cursor = this.input.keyboard.createCursorKeys();
+
+    this.textScore = this.add.text(sizes.width - 120, 10, "Score : 0", {
+      font: "25px Arial",
+      fill: "#000000",
+    });
+
+    this.textTime = this.add.text(10, 10, "Temps Restant : 30", {
+      font: "25px Arial",
+      fill: "#000000",
+    });
+
+    this.timedEvent = this.time.delayedCall(30000, this.gameOver, [], this);
+  }
+
+  update() {
+    this.remainingTime = this.timedEvent.getRemainingSeconds();
+    this.textTime.setText(`Temps Restant : ${Math.round(this.remainingTime)}`);
+
+    if (this.target.y >= sizes.height) {
+      this.target.setY(0);
+      this.target.setX(this.getRandomX());
+    }
+
+    const { left, right } = this.cursor;
+
+    if (left.isDown) {
+      this.player.setVelocityX(-this.playerSpeed);
+    } else if (right.isDown) {
+      this.player.setVelocityX(this.playerSpeed);
+    } else {
+      this.player.setVelocityX(0);
+    }
+  }
+
+  getRandomX() {
+    return Math.floor(Math.random() * 460);
+  }
+
+  targetHit() {
+    this.target.setY(0);
+    this.target.setX(this.getRandomX());
+    this.points++;
+    this.textScore.setText(`Score : ${this.points}`);
+  }
+
+  gameOver() {
+    this.sys.game.destroy(true);
+
+    gameEndScoreSpan.textContent = this.points;
+    gameWinLoseSpan.textContent = this.points >= 35 ? "Gagné !" : "Perdu !";
+    gameEndDiv.style.display = "flex";
+  }
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: { gravity: { y: speedDown }, debug: true }
+  },
+  scene: [GameScene]
+};
+
+const game = new Phaser.Game(config);
+
+gameStartBtn.addEventListener("click", () => {
+  gameStartDiv.style.display = "none";
+  game.scene.start("scene-game");
+});
